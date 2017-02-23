244c0745ffd58788fa6cd7114e1b7705dc4d2bd3
Matheus Araújo <Matheus Arajo>
2017-02-22 21:29:25 -0300

    Primeiro Comit doidooo

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,34 @@
 // CONTROLLER - Home
 module.exports = function(app){
     var HomeController = {
+        //ACTION - INDEX
         index: function(req, res){
             res.render('home/index');
-        }
+        },
+
+        //ACTION - LOGIN
+        login: function(req, res){
+
+        //Pegando os dados do formulário
+        var email  = req.body.usuario.email,
+            nome   = req.body.usuario.nome;
+
+            if(usuario && email){
+             var usuario = req.body.usuario;
+             usuario['contatos'] = [];
+             req.session.usuario = usuario;
+             res.redirect('/contatos');
+            } else {
+                res.redirect('/');
+            }
+         },
+
+         //ACTION - LOGOUT
+         logou: function(req, res){
+
+             req.session.destroy();
+             res.redirect('/');
+         }
     };
     
     return HomeController;
--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,8 @@ module.exports = function(app){
 
   var home = app.controllers.home;
   app.get('/', home.index);
+  app.get('/', entrar.login);
+  app.get('/', sair.logout);
 
 };
 
